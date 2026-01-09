1b7867829012a0f75078fac622050c26a3f8fc9b
Ton-1211 <113411399+Ton-1211@users.noreply.github.com>
2026-01-09 18:41:18 +0900

    エフェクトの再生後に非有効化する汎用スクリプトを複数エフェクトに対応

--- Cooking_Game/Assets/Suzuki/Scripts/Effect/EffectAutoDeactivation.cs
+++ Cooking_Game/Assets/Suzuki/Scripts/Effect/EffectAutoDeactivation.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class EffectAutoDeactivation : MonoBehaviour
 {
-    [SerializeField] new ParticleSystem particleSystem;
+    [Header("再生対象（自身の子）のエフェクトたち"), SerializeField] List<ParticleSystem> particleSystems;
 
     void Awake()
     {
-        if(particleSystem == null) particleSystem = GetComponent<ParticleSystem>();
+        if(particleSystems == null)
+        {
+            // 初期化
+            particleSystems = new List<ParticleSystem>();
+
+            // パーティクルを全て取得
+            ParticleSystem[] particles = GetComponentsInChildren<ParticleSystem>();
+
+            // 追加
+            particleSystems.AddRange(particles);
+        }
     }
 
     void OnEnable()
     {
-        if(particleSystem != null) StartCoroutine(DeactivateAfterDuration());
+        if(particleSystems != null) StartCoroutine(DeactivateAfterDuration());
     }
 
     /// <summary>
@@ -21,8 +31,12 @@ public class EffectAutoDeactivation : MonoBehaviour
     /// </summary>
     IEnumerator DeactivateAfterDuration()
     {
-        // エフェクトの効果時間分待機
-        yield return new WaitForSeconds(particleSystem.main.duration);
+        // エフェクトの最長効果時間を取得
+        float maxParticleTime = GameConstants.Zero;
+        foreach(ParticleSystem particle in particleSystems) if(particle.main.duration > maxParticleTime) maxParticleTime = particle.main.duration;
+
+        // エフェクトの最長効果時間分待機
+        yield return new WaitForSeconds(maxParticleTime);
 
         // 自身を無効化
         this.gameObject.SetActive(false);
