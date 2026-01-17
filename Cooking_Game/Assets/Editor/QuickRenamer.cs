0b8e6eb642da6da5b274272c9ccc70f7b67b3189
Ton-1211 <g2227047@st.t-kougei.ac.jp>
2026-01-17 16:21:34 +0900

    数字カウント機能と、カウント終了時にSEを鳴らせるように、リザルトの得点表示の実装

---
+++ Cooking_Game/Assets/Editor/QuickRenamer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+public class QuickRenamer : EditorWindow
+{
+    // Unity上部メニューの「Tools > Quick Rename」から実行する
+    [MenuItem("Tools/Quick Rename")]
+    public static void Rename()
+    {
+        // 1つ以下しか選択されていなければ何もしない
+        if (Selection.gameObjects.Length <= GameConstants.One) return;
+
+        // 選択されたオブジェクトを取得（ヒエラルキー上の順序でソートされたものを得る）
+        GameObject[] selectedObjects = Selection.gameObjects.OrderBy(targetObject  => targetObject.transform.GetSiblingIndex()).ToArray();
+
+        // Undoグループの作成と命名
+        Undo.IncrementCurrentGroup();
+        Undo.SetCurrentGroupName("Batch Rename Objects");
+        int undoGroupIndex = Undo.GetCurrentGroup();
+
+        // 選択された中でヒエラルキーの一番上にあるオブジェクトの名前を基準にする
+        string baseName = selectedObjects[GameConstants.FirstIndex].name;
+        
+        // １つ目のオブジェクトは1番目として数える
+        int baseNameIndex = GameConstants.One;
+        for(int i = 0; i < selectedObjects.Length; i++)
+        {
+            // 各オブジェクトの変更を同じグループに記録
+            Undo.RecordObject(selectedObjects[i], "Rename Object");
+
+            // 名前変更（○つ目のオブジェクトは「オブジェクト名_○」となる）
+            selectedObjects[i].name = $"{baseName}_{i + baseNameIndex}";
+        }
+
+        // Undoグループをまとめて記録
+        Undo.CollapseUndoOperations(undoGroupIndex);
+
+        Debug.Log($"{selectedObjects.Length} 個のオブジェクトを改名しました(Ctrl + Z で一括で復元できます)");
+    }
+}
--- Cooking_Game/Assets/Suzuki/Scripts/UI/NumberCountUp.cs
+++ Cooking_Game/Assets/Suzuki/Scripts/UI/NumberCountUp.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class NumberCountUp
 {
+    // アニメーション中のテキスト数
+    int currentActiveAnimationCount = GameConstants.Zero;
+
+    public bool IsAllFinished => currentActiveAnimationCount == GameConstants.Zero;
+
+
+
     /// <summary>
     /// テキストの数字のカウントアップアニメーション（アニメーション時間で固定）
     /// </summary>
     /// <param name="targetText">数字のカウントアップを行うテキスト</param>
     /// <param name="targetNum">目的の数字</param>
     /// <param name="duration">アニメーションの長さ（秒）</param>
+    /// <param name="onComplete">完了時に行う処理</param>
     /// <returns></returns>
-    public static IEnumerator CountUpNumByTime(TMPro.TextMeshProUGUI targetText, int targetNum, float duration)
+    public IEnumerator CountUpNumByTime(TMPro.TextMeshProUGUI targetText, int targetNum, float duration, System.Action onComplete = null)
     {
-        float timer = GameConstants.FirstTimerValue;
+        // アニメーション開始時
+        OnStartAnimation();
 
-        while(timer < duration)
+        try
         {
-            // 経過時間の割合から数字を求める
-            int num = (int)Mathf.Lerp(GameConstants.Zero, targetNum, timer / duration);
+            // 長さが0秒なら即終了（ゼロ除算を防ぐ）
+            if (duration == GameConstants.Zero)
+            {
+                targetText.SetText("{0}", targetNum);
+                yield break;
+            }
 
-            // 数字をテキストに適用
-            targetText.text = num.ToString();
+            float timer = GameConstants.FirstTimerValue;
 
-            timer += Time.deltaTime;
-            yield return null;
+            while (timer < duration)
+            {
+                // 経過時間の割合から数字を求める
+                int num = (int)Mathf.Lerp(GameConstants.Zero, targetNum, timer / duration);
+
+                // 数字をテキストに適用
+                targetText.SetText("{0}",num);
+
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
+            // 確実に目的の値になるようにしておく
+            targetText.SetText("{0}", targetNum);
         }
-
-        // 確実に目的の値になるようにしておく
-        targetText.text = targetNum.ToString();
+        finally
+        {
+            // アニメーション終了時
+            OnEndAnimation(onComplete);
+        }
     }
 
     /// <summary>
@@ -37,25 +63,62 @@ public class NumberCountUp
     /// <param name="targetText">数字のカウントアップを行うテキスト</param>
     /// <param name="targetNum">目的の数字</param>
     /// <param name="countSpeed">カウントの速さ（数値/秒）</param>
+    /// <param name="onComplete">完了時に行う処理</param>
     /// <returns></returns>
-    public static IEnumerator CountUpNumBySpeed(TMPro.TextMeshProUGUI targetText, int targetNum, float countSpeed)
+    public IEnumerator CountUpNumBySpeed(TMPro.TextMeshProUGUI targetText, int targetNum, float countSpeed, System.Action onComplete = null)
     {
-        int num = GameConstants.Zero;
-        float timer  = GameConstants.FirstTimerValue;
+        // アニメーション開始時
+        OnStartAnimation();
 
-        while (num < targetNum)
+        try
         {
-            // 数字のカウント
-            num = (int)(timer * countSpeed);
+            // カウント速度が0なら即終了
+            if (countSpeed == GameConstants.Zero)
+            {
+                targetText.SetText("{0}", targetNum);
+                yield break;
+            }
 
-            // 数字をテキストに適用
-            targetText.text = num.ToString();
+            int num = GameConstants.Zero;
+            float timer = GameConstants.FirstTimerValue;
 
-            timer += Time.deltaTime;
-            yield return null;
+            while (num < targetNum)
+            {
+                // 数字のカウント
+                num = (int)(timer * countSpeed);
+
+                // 数字をテキストに適用
+                targetText.SetText("{0}", num);
+
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
+            // 確実に目的の値になるようにしておく
+            targetText.SetText("{0}", targetNum);
         }
+        finally
+        {
+            // アニメーション終了時
+            OnEndAnimation(onComplete);
+        }
+    }
 
-        // 確実に目的の値になるようにしておく
-        targetText.text = targetNum.ToString();
+    /// <summary>
+    /// アニメーション開始時、再生中アニメーション数を増やす
+    /// </summary>
+    void OnStartAnimation() => currentActiveAnimationCount++;
+
+    /// <summary>
+    /// アニメーション終了時、再生中アニメーション数を減らす(0以下にならないように)
+    /// </summary>
+    /// <param name="onComplete">完了時に行う処理</param>
+    void OnEndAnimation(System.Action onComplete)
+    {
+        // カウントを減らす
+        currentActiveAnimationCount = Mathf.Max(GameConstants.Zero, --currentActiveAnimationCount);
+
+        // 渡された完了時の処理を行う
+        onComplete?.Invoke();
     }
 }
